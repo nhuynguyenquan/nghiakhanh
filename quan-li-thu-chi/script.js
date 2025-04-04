2ca78f9ba9bfc119b783e6d1af24f36a1e6aa41b
nhuynguyenquan <tranvananhuaf@gmail.com>
2025-04-05 01:43:22 +0900

    Update script.js

--- quan-li-thu-chi/script.js
+++ quan-li-thu-chi/script.js
@@ -1,6 +1,6 @@
 let transactions = [];
 
-const API_URL = "https://script.google.com/macros/s/AKfycbyAPbVzkzPfjgfmPcX_5BJMbZ1C3Aoq086iUl-xnO-JavjOh67APPR3Z44fph6eAg-v/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyWF4SAQoD_5ITo2z2abnJ8qX-s4pl62K1_dXDRW9HfLIAf8jXzPY_dcfZuLK5N7UV2/exec";
 const TELEGRAM_BOT_TOKEN = "7783089403:AAGNpG6GsdlF7VXVfPTW8Y1xQJEqBahL1PY";
 const TELEGRAM_CHAT_ID = "6249154937"; 
 //https://drive.google.com/file/d/1GFPr__AeZN9Y79AIx1hD-EgtloapHdJB/view?usp=sharing
