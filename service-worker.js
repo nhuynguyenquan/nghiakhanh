251b0b387991d92b0e01392c49974fa8e47db642
nhuynguyenquan <tranvananhuaf@gmail.com>
2025-08-02 18:33:00 +0900

    cgnhccncv

--- service-worker.js
+++ service-worker.js
@@ -1,54 +1,26 @@
 const CACHE_NAME = "nghiakhanh-cache-v1";
 const urlsToCache = [
-  "/nghiakhanh/",
-  "/nghiakhanh/index.html",
-  "/nghiakhanh/banhang.html",
-  "/nghiakhanh/manifest.json",
-  "/nghiakhanh/service-worker.js",
-  "/nghiakhanh/icons/icon-192.png",
-  "/nghiakhanh/icons/icon-512.png",
+  "/",
+  "/index.html",
+  "/banhang.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
 
-  // Các trang quản lý
-  "/nghiakhanh/quan-li-thu-chi/",
-  "/nghiakhanh/quan-li-thu-chi/index.html",
-  "/nghiakhanh/quan-li-thu-chi/thu-chi-gia-dinh.html",
+  "/quan-li-thu-chi/",
+  "/quan-li-thu-chi/index.html",
+  "/quan-li-thu-chi/thu-chi-gia-dinh.html",
 
-  "/nghiakhanh/quan-li-kho/",
-  "/nghiakhanh/quan-li-kho/index.html",
+  "/quan-li-kho/",
+  "/quan-li-kho/index.html",
 
-  "/nghiakhanh/quan-li-nv/",
-  "/nghiakhanh/quan-li-nv/index.html",
+  "/quan-li-nv/",
+  "/quan-li-nv/index.html",
 
-  "/nghiakhanh/quan-li-menu/",
-  "/nghiakhanh/quan-li-menu/index.html",
+  "/quan-li-menu/",
+  "/quan-li-menu/index.html",
 
-  "/nghiakhanh/quan-li-chi-nhanh/",
-  "/nghiakhanh/quan-li-chi-nhanh/index.html",
+  "/quan-li-chi-nhanh/",
+  "/quan-li-chi-nhanh/index.html"
 ];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
-});
