11bb0874d4e506adb36b2e0c8b85f4eb99065f5c
nhuynguyenquan <tranvananhuaf@gmail.com>
2026-01-11 14:52:00 +0900

    aaaadgh

--- test/caphe/js/config.js
+++ test/caphe/js/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
   SHOP_ID: 'demo_cafe',
-  API_URL: 'https://script.google.com/macros/s/XXXX/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbwueCoBff5nHYZGJHvOb_pExlZEpcmYslUEPnuqNEh0qh8GrlcuaZUMsr8gBT2E_Tf92g/exec'
 };
